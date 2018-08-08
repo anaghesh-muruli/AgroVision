ee5d2f71675feeb57794dff72b2a84c65f23d9e2
Anaghesh Muruli <anagheshm@gmail.com>
2018-08-08 15:02:32 +0530

    Add files via upload

---
+++ AgroVision_ardiuno_code.ino
@@ -0,0 +1,59 @@
+#include <SoftwareSerial.h>
+ 
+SoftwareSerial Genotronex(10, 11); // TX, RX
+
+#define MAX_BUFFER 4
+ 
+int motor = 9;    
+int speed = 50;
+char data;
+char* buffer;
+boolean receiving = false;
+int pos;
+
+void setup()  { 
+  Genotronex.begin(9600);
+  Genotronex.println("Bluetooth On");
+  pinMode(motor, OUTPUT);
+  
+   buffer = new char[MAX_BUFFER];
+} 
+
+void loop()  
+  
+   if (Genotronex.available()){
+        
+        data=Genotronex.read();
+        
+         switch(data) {
+            //3: End of transmission pu
+            case 3:  receiving = false;  
+                    speed = buffer2int(buffer);
+                               
+                    Genotronex.print("Received: ");
+                    Genotronex.print(buffer);
+                    Genotronex.print(", Speed: ");
+                    Genotronex.println(speed);
+
+                     break; //end message
+            default: if (receiving == false) resetData();
+                    buffer[pos] = data;
+                    pos++;
+                     receiving = true;          
+          }
+   }  
+  digitalWrite(motor, speed); 
+  delay(10);                            
+}
+
+ void resetData(){
+   for (int i=0; i<=pos; i++) buffer[i] = 0; 
+   pos = 0;
+}Q
+    
+int buffer2int(char* buffer){
+  int i;
+  sscanf(buffer, "%d", &i);
+  return i;
+}
+
